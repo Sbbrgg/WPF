172a3faa77d6c2e8bc7a6aa79214eaf363480b18
un <un@DESKTOP-JAU235M>
2026-04-17 18:05:04 +0300

    Add project files.

---
+++ WPF/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			//btnStart.Content = btnStart.Content.ToString() == "Stop" ? "Start" : "Stop";
+			(sender as Button).Content = ((Button)sender).Content.ToString() == "Stop" ? "Start" : "Stop";
+			//as приводит переменную к типу данных
+		}
+    }
+}
